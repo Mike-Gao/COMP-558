e5600656c5dc11e729f12b71982d82f301c24239
Mike Gao <contact@mikegao.net>
2020-10-07 00:02:39 -0400

    Fix A1 issues

--- Assignments/1/Q1_demosaic.m
+++ Assignments/1/Q1_demosaic.m
@@ -10,32 +10,32 @@ B = zeros(row, col);
 
 % fill red
 i = 2:2:row;
-j = 1:2:col;
+j = 2:2:col;
 R(i,j) = im(i,j,1);
-red = uint8(cat(3, R, zeros(row, col), zeros(row,col)));
+red = uint8(cat(3, R, zeros(row, col), zeros(row, col)));
 subplot(1,5,1);
 image(red);
 title("red");
 
 
 % fill green
-i = 1:2:row;
+i = 2:2:row;
 j = 1:2:col;
 G(i,j) = im(i,j,2);
-i = 2:2:row;
+i = 1:2:row;
 j = 2:2:col;
 G(i,j) = im(i,j,2);
-green = uint8(cat(3, zeros(row, col), G, zeros(row,col)));
+green = uint8(cat(3, zeros(row, col), G, zeros(row, col)));
 subplot(1,5,2);
 image(green);
 title("green");
 
 
 % fill blue
-i = 2:2:row;
+i = 1:2:row;
 j = 1:2:col;
 B(i,j) = im(i,j,3);
-blue = uint8(cat(3, zeros(row, col), zeros(row,col), B));
+blue = uint8(cat(3, zeros(row, col), zeros(row, col), B));
 subplot(1,5,3);
 image(blue);
 title("blue");

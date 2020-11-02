aab84779e152dde80a8c62a19af510f327f269b7
Mike Gao <contact@mikegao.net>
2020-11-02 00:05:21 -0500

    Assignment 2 Completed

--- Assignments/2/A2.m
+++ Assignments/2/A2.m
@@ -1,6 +1,8 @@
+close all
+clear
 % Load Image %
-im_syn = rgb2gray(imread('synthetic.jpg'));
-im_nat = rgb2gray(imread('mtl.jpg'));
+im_syn = rgb2gray(imread('nat.jpg'));
+im_nat = rgb2gray(imread('nat.jpg'));
 % Q7 - resized image
 % im_syn = rgb2gray(imresize(imread('synthetic.jpg'),0.8));
 gss_syn = getgss(im_syn);
@@ -22,7 +24,7 @@ for i = 1:16
     title(sprintf("Gaussian %i: Sigma value %f",i+1,sig));
 end 
 
-nat = rgb2gray(imread('mtl.jpg'));
+nat = rgb2gray(imread('nat.jpg'));
 figure
 title("natural");
 for i = 1:16
@@ -103,8 +105,11 @@ for i = 1:size(keypoints,1)
     dy = conv2(neighbors,yfilter,'valid');
     dxx = conv2(dx, xfilter, 'valid');
     dyy = conv2(dy, yfilter, 'valid');
-    dxy = conv2(dy, yfilter, 'valid');
+    dxy = conv2(dx, yfilter, 'valid');
     hessian = [dxx, dxy; dxy, dyy];
+%     if x>=435 && x<450 && y>=165 && y <=175
+%        disp(hessian)
+%     end
     if (trace(hessian)^2/det(hessian) < threshold && det(hessian) > 0)
         keypoints_new(cur,:) = keypoints(i,:);
         cur = cur + 1;
@@ -115,7 +120,7 @@ for i = 1:size(keypoints,1)
 end
 keypts_syn = keypoints_new(1:cur-1,:);
 
-
+% commented out to save some ram space
 % figure
 % imagesc(im_nat);
 % colormap(gray);
@@ -135,7 +140,7 @@ keypts_syn = keypoints_new(1:cur-1,:);
 %     dy = conv2(neighbors,yfilter,'valid');
 %     dxx = conv2(dx, xfilter, 'valid');
 %     dyy = conv2(dy, yfilter, 'valid');
-%     dxy = conv2(dy, yfilter, 'valid');
+%     dxy = conv2(dx, yfilter, 'valid');
 %     hessian = [dxx, dxy; dxy, dyy];
 %     if (trace(hessian)^2/det(hessian) < threshold && det(hessian) > 0)
 %         keypoints_new(cur,:) = keypoints(i,:);
@@ -150,43 +155,47 @@ keypts_syn = keypoints_new(1:cur-1,:);
 figure;
 imagesc(im_syn);
 colormap(gray);
-binhists = zeros(300, 36);
-sift_feature_dominant = zeros(1000,4);
+pos = 1;
+cur = 1;
+histogram = zeros(size(gss_syn,1),size(gss_syn,2),36);
+sift_feature_dominant = zeros(length(gss_syn)*36,4);
 keypoints = keypts_syn;
 for i = 1:size(keypoints,1)
     x = keypoints(i,1);
     y = keypoints(i,2);
     l = keypoints(i,3);
-    sig_round = round(getsig(l));
-    neighbors = gss_syn(x-sig_round-1:x+sig_round+1, y-sig_round-1:y+sig_round+1,l);
+    sig_round = ceil(getsig(l));
+    neighbors = gss_syn(y-sig_round-1:y+sig_round+1, x-sig_round-1:x+sig_round+1,l);
+    viscircles([x, y], sig_round, 'Color','r','LineWidth',1);
     dx = conv2(neighbors,xfilter,'valid');
     dy = conv2(neighbors,yfilter,'valid');
-    mag = sqrt(dx.*dx + dy.*dy);
-    gauss = fspecial('gaussian', floor(2*getsig(l)), 1.5*getsig(l));
+    mag = sqrt(dx.^2 + dy.^2);
+    gauss = fspecial('gaussian', 2*sig_round+1, 1.5*getsig(l));
     mag_weighted = mag .* gauss;
-    dir = atan2d(-dx, dy);
+    dir = atan2d(dy, dx);
+    dir = dir + 5; % rounding
     dir(dir<0) = dir(dir<0) + 360;
-    bin = round(dir/10) + 1;
-    bin(bin==37) = 1;
     bin_hist = zeros(1,36);
-    for j = 1:size(mag_weighted,1)
-        bin_hist(bin(j)) = bin_hist(bin(j)) + mag_weighted(j);
+    bin = floor(dir./10);
+    for j = 1:numel(bin)
+        bin_hist(bin(j)+1) = bin_hist(bin(j)+1) + mag_weighted(j);
     end
-    binhists(pts,:) = bin_hist;
-    pos = pos + 1;
-    large_vals = binhist( binhist > 0.8 * max(binhist) )';
-    for k=1:length(large_vals)
-        idx = find(binhist == larges(k));
-        ang = (idx-1)*10;
-        sift_feature_dominant(cur,:) = [x y l ang];
-        cur = cur + 1;
-        delta_x = 2*getsig(l)*cos(-deg2rad(ang));
-        delta_y = 2*getsig(l)*sin(-deg2rad(ang));
-        line([y y+delta_y], [x x+delta_x],'Color', 'r', 'LineWidth',2);
+    histogram(y,x,:) = bin_hist;
+    lim = max(bin_hist)*0.8;
+    for k=1:length(bin_hist)
+        if bin_hist(k) > lim
+            ang = (k-1)*10;
+            sift_feature_dominant(cur,:) = [x y l ang]; %save for later use
+            cur = cur + 1;
+            delta_x = -2*getsig(l)*cosd(ang);
+            delta_y = -2*getsig(l)*sind(ang);
+            line([x x+delta_x], [y y+delta_y],'Color', 'r', 'LineWidth',2);
+        end
     end
 end
 
-    
+
+
 % Q7 - Scale Invariance
 % Change im_syn to rgb2gray(imresize(imread('synthetic.jpg'),0.8)), see
 % above.
@@ -201,7 +210,7 @@ function sig = getsig(i)
 end
 
 function gss = getgss(im)
-   gss = zeros(size(im,1), size(im,2),17, 'uint8');
+   gss = zeros(size(im,1), size(im,2),17);
    for i = 1:17
        sig = getsig(i);
        gauss = fspecial('gaussian', floor(sig*2), sig);
@@ -234,7 +243,7 @@ function hs = harris(sig,im)
 end
 
 function dog = getdog(gss)
-dog = zeros(size(gss,1), size(gss,2), size(gss,2), 'uint8');
+dog = zeros(size(gss,1), size(gss,2), size(gss,2));
     for i = 1:16
         dog(:,:,i) = gss(:,:,i) - gss(:,:,i+1);
     end
@@ -263,8 +272,9 @@ for i = 2:15
         end
     end
 end
-pts = pts(pts(:,3) ~= 0,:); 
+pts = pts(1:cur-1,:); 
 end
+
 
 function plotimage(im,map,t)
     imagesc(im)

1875b10f567c81c56f44882c46568e3214ebd2e3
Mike Gao <contact@mikegao.net>
2020-11-27 17:42:58 -0500

    Added GetHNormalized, GenerateA

--- Assignments/3/Q1.m
+++ Assignments/3/Q1.m
@@ -21,17 +21,20 @@ matched_2 = valid_pts_2(indexPairs(:,2));
 figure;
 showMatchedFeatures(pic_1,pic_2,matched_1,matched_2);
 
-
-idx_random_pair = randperm(length(indexPairs), 4);
-
-out = getNorm(matched_1);
+for i=1:100
+    idx_random_pair = randperm(length(indexPairs), 4);
+    matched_1_norm = getNorm(matched_1);
+    matched_2_norm = getNorm(matched_2);
+    mat_a = generateA(4, matched_1, matched_2);
+    h_norm = getHNormalized(mat_a);
+end
 
 function norm = getNorm(match)
     mat = match.Location;
     n = size(mat, 1);
     mat_mean = mean(mat);
     new_mat = (mat - mat_mean) .^ 2;
-    mat_sum = sum(new_mat) / (2*n);
+    mat_sum = sum(new_mat, 'all') / (2*n);
     std = sqrt(mat_sum);
     T = [1 0 mat_mean(1); 0 1 mat_mean(2); 0 0 1];
     S = [1/std 0 0; 0 1/std 0; 0 0 1];
@@ -49,4 +52,10 @@ function A = generateA(size, matched_1, matched_2)
         A(2*i - 1, :) = [x_pos y_pos 1 0 0 0 -x_pos_2*x_pos -x_pos_2*y_pos -x_pos_2];
         A(2*i, :) = [0 0 0 x_pos y_pos 1 -y_pos_2*x_pos -y_pos_2*y_pos -y_pos_2]; 
     end
+end
+
+function H = getHNormalized(mat_in)
+    [U, S, V] = svd(mat_in);
+    sol = V(:,end)';
+    H = reshape(sol,3,3);
 end
